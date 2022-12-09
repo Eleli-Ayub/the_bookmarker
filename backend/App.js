ecaf6bdf6c72af56ba61005bdf7d34ad51fc3992
Eleli-Ayub <lesosioayub@gmail.com>
2022-12-09 23:59:40 +0300

    Made the appjs

---
+++ backend/App.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRouter = require("./API/Authapi");
+
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URL, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connection has been succesful");
+  }
+});
+
+app.use("/users", userRouter);
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is running on port " + port);
+  }
+});
