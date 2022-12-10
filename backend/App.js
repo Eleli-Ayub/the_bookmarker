5da5480f28d21be73681d8b30294252ae247506c
Eleli-Ayub <lesosioayub@gmail.com>
2022-12-10 17:30:06 +0300

    released connecting to db

--- backend/App.js
+++ backend/App.js
@@ -13,13 +13,13 @@ app.get("/", (req, res) => {
 });
 
 // connect to the database
-// mongoose.connect(process.env.MONGO_URL, (error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Connection to db succesfull");
-//   }
-// });
+mongoose.connect(process.env.MONGO_URL, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connection to db succesfull");
+  }
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/task", verifyJwt, taskRouter);
