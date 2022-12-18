262df9525029dc5f680db23b58d5867031ca86b8
Eleli-Ayub <lesosioayub@gmail.com>
2022-12-18 11:03:56 +0300

    Added icons to tasks

--- bookmarker/Components/Homepage/TaskListItem.tsx
+++ bookmarker/Components/Homepage/TaskListItem.tsx
@@ -2,12 +2,29 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Taskslist_I } from "../../Global/Types";
 import { Homepagestyles } from "../../Styles/Homepage";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 
-const TaskListItem = ({ number, text }: Taskslist_I) => {
+const TaskListItem = ({ number, text, isDone }: Taskslist_I) => {
   return (
     <TouchableOpacity style={Homepagestyles.taskWrapper}>
       <Text style={Homepagestyles.taskNumberText}>{number}</Text>
-      <View style={Homepagestyles.taskCheckbox}></View>
+      <View style={Homepagestyles.taskCheckbox}>
+        {isDone ? (
+          <Ionicons
+            style={Homepagestyles.taskCheckbox}
+            name="ios-checkmark-done-sharp"
+            size={20}
+            color="green"
+          />
+        ) : (
+          <MaterialIcons
+            style={Homepagestyles.taskCheckbox}
+            name="remove-done"
+            size={20}
+            color="red"
+          />
+        )}
+      </View>
       <Text style={Homepagestyles.taskTitleText}>{text}</Text>
     </TouchableOpacity>
   );
